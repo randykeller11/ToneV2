c834baac8ab3677a6a0c677db59aaeb11f5a75eb
Randy <randykeller1193@gmail.com>
2020-11-01 23:01:22 -0500

    added colors

--- src/PlayerPad.js
+++ src/PlayerPad.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import './PlayerPad.css';
 
-function PlayerPad() {
+function PlayerPad({colIndex, rowIndex}) {
+
+    const colors = ["#4570E6","#5DADEC", "#0095B7", "#76D7EA"]
+
+    const mystyle = {
+        "border-radius": "10%",
+        backgroundColor: colors[colIndex],
+      }
     return (
-        <div className="PlayerPad">
+        <div className="PlayerPad" style={mystyle}>
 
         </div>
     );
