99d216e6405c9faaf96084d94eb76ca680c6aff0
Randy <randykeller1193@gmail.com>
2020-11-03 21:22:19 -0500

    shortened classnames and improved transport buttons

--- src/Tester.js
+++ src/Tester.js
@@ -5,7 +5,6 @@ import { dataLayer } from "./App";
 import PlayerPad from "./PlayerPad";
 
 function Tester() {
-
   const [colorTheme, setColorTheme] = useState(0);
 
   const {
@@ -23,13 +22,23 @@ function Tester() {
     const sortedPlayers = [];
     const localPlayers = [...players];
     sortedPlayers.push(localPlayers.filter((player, index) => index < 4));
-    sortedPlayers.push(localPlayers.filter((player, index) => index >= 4 && index < 8));
-    sortedPlayers.push(localPlayers.filter((player, index) => index >= 8 && index < 12));
-    sortedPlayers.push(localPlayers.filter((player, index) => index >= 12 && index < 16));
-    return sortedPlayers.map((playerRow, colIndex)=>(
+    sortedPlayers.push(
+      localPlayers.filter((player, index) => index >= 4 && index < 8)
+    );
+    sortedPlayers.push(
+      localPlayers.filter((player, index) => index >= 8 && index < 12)
+    );
+    sortedPlayers.push(
+      localPlayers.filter((player, index) => index >= 12 && index < 16)
+    );
+    return sortedPlayers.map((playerRow, colIndex) => (
       <div className="donut__padRow">
-        {playerRow.map((player, rowIndex)=>(
-          <PlayerPad colorTheme={colorTheme} rowIndex={rowIndex} colIndex={colIndex}/>
+        {playerRow.map((player, rowIndex) => (
+          <PlayerPad
+            colorTheme={colorTheme}
+            rowIndex={rowIndex}
+            colIndex={colIndex}
+          />
         ))}
       </div>
     ));
@@ -38,38 +47,36 @@ function Tester() {
   const forwardSoundToggle = () => {
     const newValue = (colorTheme + 1) % 3;
     setColorTheme(newValue);
-  }
+  };
 
   return (
-    <div className="donut__practice">
-
-      <div className="donut__transportButtons">
-        <div className="donut__transportButtons__buttonBox">
-          <button>⏯</button>
-          <h5>Play</h5>
+    <div className="practice">
+      <div className="transportButtons">
+        <div className="transportButtons__buttonBox">
+          <div className="transportButtons__buttonBox__circle"></div>
+          <h5 id="buttonBox__description">Play</h5>
         </div>
-        <div className="donut__transportButtons__buttonBox">
-          <button>⏺</button>
-          <h5>Record</h5>
+        <div className="transportButtons__buttonBox">
+          <div className="transportButtons__buttonBox__circle"></div>
+          <h5 id="buttonBox__description">Record</h5>
         </div>
-        <div className="donut__transportButtons__buttonBox">
-          <button>↩️</button>
-          <h5>Undo</h5>
+        <div className="transportButtons__buttonBox">
+          <div className="transportButtons__buttonBox__circle"></div>
+          <h5 id="buttonBox__description">Undo</h5>
         </div>
-        <div className="donut__transportButtons__buttonBox">
-          <button>🎼</button>
-          <h5>Snap</h5>
+        <div className="transportButtons__buttonBox">
+          <div className="transportButtons__buttonBox__circle"></div>
+          <h5 id="buttonBox__description">Snap</h5>
         </div>
       </div>
-      <div className="donut__progressBar">
+      <div className="progressBar">
         <LinearProgress variant="determinate" value={25} />
       </div>
-      <div className="donut__playerPads">{makePlayPads()}</div>
-      <div className="donut__toggleSounds">
+      <div className="playerPads">{makePlayPads()}</div>
+      <div className="toggleSounds">
         <button onClick={forwardSoundToggle}>New Sound Bank</button>
       </div>
-      <div className="donut__toggleMode">
-      </div>
+      <div className="toggleMode"></div>
     </div>
   );
 }
