1cc88ae133982936033660de0899d8ccb8b94b97
Randy <randykeller1193@gmail.com>
2020-11-05 04:33:44 -0500

    connected click and key press event listeners. new bug where pads shuffle

--- src/PlayerPad.js
+++ src/PlayerPad.js
@@ -1,6 +1,8 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { dataLayer } from "./App";
 import "./PlayerPad.css";
+import * as Tone from "tone";
+
 
 function PlayerPad({ colorTheme, colIndex, rowIndex }) {
   
@@ -16,17 +18,78 @@ function PlayerPad({ colorTheme, colIndex, rowIndex }) {
     recordings,
   } = useContext(dataLayer);
 
+  const padIndex = (colIndex * 4) + rowIndex;
+
   const colors = [
     ["#4570E6", "#5DADEC", "#76D7EA"],
     ["#FD3A4A", "#FF8866", "#FF9980"],
     ["#FFFF66", "#BEE64B", "#3AA655"],
   ];
 
-  const mystyle = {
+  const myStyleInactive = {
     border: "2px solid darkgray",
     backgroundColor: colors[colorTheme][Math.floor(Math.random() * 3)],
   };
-  return <div className="PlayerPad" style={mystyle}></div>;
+
+  const myStyleActive = {
+    border: "2px solid darkgray",
+    backgroundColor: "black",
+  };
+
+  
+
+  const [keyPressedDown, keyPressedUp, setKeyPressedUp] = listeners[padIndex];
+
+  const isActive = isActiveArray[padIndex].activeState;
+
+  const transportTime = Tone.Transport.position;
+
+  const makeButtonActive = () => {
+    let localArrayActive = [...isActiveArray];
+    const updatedStates = localArrayActive.map((player) =>
+      player.id === padIndex ? { id: padIndex, activeState: true } : player
+    );
+    setIsActive(updatedStates);
+  };
+
+  const downHandler = () => {
+    makeButtonActive();
+    console.log(padIndex, "down", transportTime);
+    players[padIndex].start();
+  };
+
+  const upHandler = () => {
+    if (isActive) {
+      let localArray = [...isActiveArray];
+      const updatedStates = localArray.map((player) =>
+        player.id === padIndex ? { id: padIndex, activeState: false } : player
+      );
+      setIsActive(updatedStates);
+      // console.log(index, "up", transportTime);
+    }
+  };
+
+  useEffect(() => {
+    if (keyPressedDown) {
+      downHandler();
+    }
+  }, [keyPressedDown]);
+
+  useEffect(() => {
+    if (keyPressedUp) {
+      upHandler();
+      setKeyPressedUp(false);
+    }
+  }, [keyPressedUp]);
+
+
+
+
+  return <div 
+  onMouseDown={downHandler}
+  onMouseUp={upHandler}
+  onMouseLeave={upHandler}
+  className="PlayerPad" style={isActive ? myStyleActive : myStyleInactive}></div>;
 }
 
 export default PlayerPad;
