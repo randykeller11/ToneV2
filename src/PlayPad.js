50988c4d9d3221a2a11240a15d814c0092473b55
Randy <randykeller1193@gmail.com>
2020-12-08 12:28:57 -0500

    fixed double tap pad bug

--- src/PlayPad.js
+++ src/PlayPad.js
@@ -1,53 +1,65 @@
-import React,{useState, useContext, useEffect} from 'react';
-import './PlayPad.css';
-import {presetBankData} from './PresetBank0';
+import React, { useState, useContext, useEffect } from "react";
+import "./PlayPad.css";
+import { presetBankData } from "./PresetBank0";
 
-function PlayPad({padColor, padIndex}) {
+function PlayPad({ padColor, padIndex }) {
+  const { dispatch, currentTrack, isActiveArray, players } = useContext(
+    presetBankData
+  );
 
-    const {dispatch, currentTrack, isActiveArray, players} = useContext(presetBankData);
+  const [isActive, setisActive] = useState(false);
 
-    const [isActive, setisActive] = useState(false);
+  const padLocation = { padIndex: padIndex, trackIndex: currentTrack };
 
-    const padLocation = {padIndex: padIndex, trackIndex: currentTrack}
+  const inactiveStyle = {
+    border: "2px solid darkgray",
+    backgroundColor: padColor,
+    opacity: "62%",
+  };
 
-    const inactiveStyle = {
-        border: "2px solid darkgray",
-        backgroundColor: padColor,
-        opacity: "62%",
-      };
-    
-      const activeStyle = {
-        border: "4px solid dimgray",
-        backgroundColor: padColor,
-        opacity: "100%",
-      };
+  const activeStyle = {
+    border: "4px solid dimgray",
+    backgroundColor: padColor,
+    opacity: "100%",
+  };
 
-      const activeTrackTarget = isActiveArray.find((track) => track.trackIndex === currentTrack);
+  const activeTrackTarget = isActiveArray.find(
+    (track) => track.trackIndex === currentTrack
+  );
 
-      const activePadTarget = activeTrackTarget.activeArray.find((pad)=> pad.key === padIndex);
+  const activePadTarget = activeTrackTarget.activeArray.find(
+    (pad) => pad.key === padIndex
+  );
 
-      useEffect(()=>{
-       setisActive(activePadTarget.isActive);
-      },[activePadTarget])
+  const playTargetPlayer = (player) => {
+    if (player.state === "started") {
+      player.stop();
+    }
+    player.start();
+  };
 
-      const downHandler = () => {
-        dispatch({type: 'activate', payload: padLocation});
-        players[currentTrack][padIndex].start();
-      }
+  useEffect(() => {
+    setisActive(activePadTarget.isActive);
+  }, [activePadTarget]);
 
-      const upHandler = () => {
-        dispatch({type: 'deactivate', payload: padLocation});
-      }
+  const downHandler = () => {
+    dispatch({ type: "activate", payload: padLocation });
+    playTargetPlayer(players[currentTrack][padIndex]);
+  };
 
-    return (
-        <div className="playPad"
-        style={isActive ? activeStyle : inactiveStyle}
-        onMouseDown={downHandler}
-        onMouseUp={upHandler}
-        onMouseLeave={upHandler}
-        >
-        </div>
-    );
+  const upHandler = () => {
+    dispatch({ type: "deactivate", payload: padLocation });
+  };
+
+  return (
+    <div
+      className="playPad"
+      style={isActive ? activeStyle : inactiveStyle}
+      onMouseDown={downHandler}
+      onMouseUp={upHandler}
+      onMouseLeave={upHandler}
+    ></div>
+  );
 }
 
 export default PlayPad;
